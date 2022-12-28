18ef7d2bb8c7b74ecd686b61d9967f4c4c281b31
Forrest Henry <forresthenry@Davids-iMac.local>
2022-12-27 16:44:32 -0800

    semi working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 (function($) {
   $(document).on("click", "#simulate_button", function() {
+    $("#loading_indicator").css("display", "flex");
+
     var deckInputString = $("#deck_input").val();
 
     var deck = manaBaseTextToDeck(deckInputString);
 
     console.log(deck);
-    var results = simulateManaBase(deck);
+    var targetManaCost = parseManaCost($("#mana_cost").val());
+    var results = simulateManaBase(deck, targetManaCost);
+
+    var maxResult = 1;
+    for (var i = 0; i < results.length; i++) {
+      if (results[i] > maxResult) { maxResult = results[i] }
+    }
+    // Render results
+    for (var i = 0; i < results.length; i++) {
+      var height = (results[i] / maxResult) * 80;
+      $("#bar_graph_column_" + i).css("height", height + "%");
+    }
+    $("#loading_indicator").css("display", "none");
   })
 })(jQuery);
--- assets/js/mtg_mana_wizard.js
+++ assets/js/mtg_mana_wizard.js
@@ -2,10 +2,42 @@
   MTG Mana Wizard
   Forrest Henry
 */
+/*
+colors=b behavior=basic fetchable count=9
+colors=r behavior=basic fetchable count=9
+colors=g behavior=basic fetchable count=9
+colors=br count=1
+colors=rg count=1
+colors=bg count=1
+*/
 
-function simulateManaBase(deck) {
-  var numSimulations = 5, // 10000
-      numTurns = 10;
+function parseManaCost(manaCostString) {
+  manaCostString = manaCostString.toLowerCase();
+
+  var manaCost = {};
+
+  manaCost.c = parseInt(manaCostString);
+  if (isNaN(manaCost.c)) manaCost.c = 0;
+
+  ['w', 'u', 'b', 'r', 'g'].forEach(color => {
+    var regex = new RegExp(color, "g");
+    manaCost[color] = (manaCostString.match(regex) || []).length
+  });
+
+  console.log(manaCost);
+
+  return manaCost;
+}
+
+function simulateManaBase(deck, targetManaCost) {
+  var numSimulations = 10000,
+      numTurns = 10,
+      turnsToCasts = [];
+  
+  // Make an array of zeros, one more than turn length (final value represents a sim that couldn't cast)
+  for (var i = 0; i <= numTurns; i++) {
+    turnsToCasts[i] = 0;
+  }
 
   for (var simulationNum = 0; simulationNum < numSimulations; simulationNum++) {
     console.log("Running simulation " + (simulationNum + 1) + " of " + numSimulations + "...")
@@ -15,16 +47,113 @@ function simulateManaBase(deck) {
         hand = [];
     
     // Draw two cards
-    hand.push(deck.splice(Math.floor(Math.random() * currentDeck.length), 1)[0])
-    hand.push(deck.splice(Math.floor(Math.random() * currentDeck.length), 1)[0])
+    hand.push(currentDeck.splice(Math.floor(Math.random() * currentDeck.length), 1)[0])
+    hand.push(currentDeck.splice(Math.floor(Math.random() * currentDeck.length), 1)[0])
 
-    for (var turnNum = 1; turnNum <= numTurns; turnNum++) {
+    for (var turnNum = 0; turnNum < numTurns; turnNum++) {
       // Draw a card
-      hand.push(deck.splice(Math.floor(Math.random() * currentDeck.length), 1)[0])
+      hand.push(currentDeck.splice(Math.floor(Math.random() * currentDeck.length), 1)[0])
+
+      // Find a card with the best castability
+      var bestIndex = -1,
+          bestScore = 10000;
+      for (var i = 0; i < hand.length; i++) {
+        var landInHand = hand[i],
+            virtualBattlefield = JSON.parse(JSON.stringify(battlefield));
+        virtualBattlefield.push(landInHand);
+
+        var scoreForThisLand = castabilityScore(targetManaCost, virtualBattlefield);
+
+        if (scoreForThisLand < bestScore) {
+          bestScore = scoreForThisLand;
+          bestIndex = i;
+        }
+      }
+
+      // Play that land
+      var landToPlay = hand.splice(bestIndex, 1)[0];
+      if (!landToPlay.enterTapped) {
+        battlefield.push(landToPlay);
+      }
+
+      // Check if we met our castability score
+      //if (castabilityScore(targetManaCost, battlefield) === 0 && battlefield.length === 2) { debugger }
+      if (castabilityScore(targetManaCost, battlefield) === 0) {
+        turnsToCasts[turnNum]++;
+        break;
+      } else if (turnNum === numTurns - 1) {
+        // If it's the last turn and we haven't been able to cast, report in the final slot
+        turnsToCasts[numTurns]++;
+      }
+
+      // Play the land "late" if it enters tapped
+      if (landToPlay.enterTapped) {
+        battlefield.push(landToPlay);
+      }
     }
   }
 
   console.log("Simulation finished");
+  console.log(turnsToCasts);
+  return turnsToCasts;
+}
+
+// Calculate the "castability" of a given mana cost. If the cost can be payed, the score is zero.
+// Points are added for each missing mana.
+function castabilityScore(manaCost, battlefield) {
+  var score = 0,
+      cmc = manaCost.c + manaCost.w + manaCost.u + manaCost.b + manaCost.r + manaCost.g;
+  if (cmc > battlefield.length) {
+    // 1 point for each missing mana
+    score += cmc - battlefield.length;
+  }
+
+  // Check that we have enough of each color
+  var baseAvailability = colorAvailablity(battlefield),
+      baseDifference = costToAvailabilityDifference(manaCost, baseAvailability);
+
+  score += baseDifference;
+
+  if (baseDifference === 0) {
+    // Check that removing a land doesn't affect difference too much
+    for (var i = 0; i < battlefield.length; i++) {
+      // Copy to a new battlefield, then remove a land from it 
+      var virtualBattlefield = JSON.parse(JSON.stringify(battlefield));
+      virtualBattlefield.splice(i, 1);
+
+      var virtualAvailability = colorAvailablity(virtualBattlefield),
+          virtualDifference = costToAvailabilityDifference(manaCost, virtualAvailability);
+
+      if (virtualDifference > 1) {
+        score += virtualDifference - 1;
+      }
+    }
+  }
+
+  return score;
+}
+
+function colorAvailablity(battlefield) {
+  sums = { w: 0, u: 0, b: 0, r: 0, g: 0 }
+
+  battlefield.forEach(land => {
+    land.colors.forEach(color => {
+      sums[color]++;
+    });
+  });
+
+  return sums;
+}
+
+function costToAvailabilityDifference(manaCost, colorAvailablity) {
+  var difference = 0;
+  ['w', 'u', 'b', 'r', 'g'].forEach(color => {
+    if (manaCost[color] > colorAvailablity[color]) {
+      difference += manaCost[color] - colorAvailablity[color];
+    }
+  });
+
+  return difference;
 }
 
 // Format:
